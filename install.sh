b4e607f5d00559ecfa150be4923a276f7b24b34d
Rhydian Jenkins <rhydianjenkins@pop-os.localdomain>
2021-06-20 18:31:40 +0100

    Warning message added to install script, as well as the ability to execute it from anywhere

--- install.sh
+++ install.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
 
-# Just copy and erase all dotfiles to $HOME
-cp -f ./dotfiles/.[a-z]* ~/
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+read -r -p "WARNING: You're about to relace your current dotfiles with these new ones. Are you sure? [y/N] " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+then
+	# Just copy and erase all dotfiles to $HOME
+	cp -f $SCRIPT_DIR/dotfiles/.[a-z]* ~/
+	echo "Dotfiles replaced"
+else
+	echo "Exiting without executing"
+fi
+
