fe32cc5c1401ad92f7d7c88b46b5186ad00664dd
Rhydian Jenkins <rhydz@msn.com>
2022-02-21 22:25:18 +0000

    Files now linked, not copied

--- install.sh
+++ install.sh
@@ -5,9 +5,9 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 read -r -p "WARNING: You're about to relace your current dotfiles with these new ones. Are you sure? [y/N] " response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
 then
-	# Just copy and erase all dotfiles to $HOME
-	cp -f $SCRIPT_DIR/dotfiles/.[a-z]* ~/
-	echo "Dotfiles replaced"
+	# Just link and erase all dotfiles to $HOME
+	ln -sf $SCRIPT_DIR/dotfiles/.[a-z]* ~/
+	echo "Dotfiles linked"
 else
 	echo "Exiting without executing"
 fi
