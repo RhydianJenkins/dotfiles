155204404f127e4db32781f0753e981450cb8af8
Rhydian Jenkins <rhydz@msn.com>
2022-04-24 20:07:58 +0100

    Which key registers added for some leader keys

--- nvim/plugins/whichkey.lua
+++ nvim/plugins/whichkey.lua
@@ -1,4 +1,6 @@
-require("which-key").setup {
+local wk = require("which-key")
+
+wk.setup {
     plugins = {
         marks = true, -- shows a list of your marks on ' and `
         registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
@@ -63,3 +65,21 @@ require("which-key").setup {
         v = { "j", "k" },
     },
 }
+
+-- Register custom names for keybindings
+wk.register({
+    ["<leader>"] = {
+        t = {
+            name = "Telescope/Test",
+        },
+        r = {
+            name = "Replace/Rename"
+        },
+        w = {
+            name = "Window Nav"
+        },
+        c = {
+            name = "Commenter/Code Action"
+        }
+    },
+})
