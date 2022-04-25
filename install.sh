8e88ce735b4a7f17d76d4cea712b1a2fa9e137a8
Rhydian Jenkins <rhydz@msn.com>
2022-04-25 22:20:59 +0100

    Install process improvments

--- install.sh
+++ install.sh
@@ -11,12 +11,9 @@ fi
 # Just link and erase all dotfiles to $HOME
 ln -sf $SCRIPT_DIR/dotfiles/.[a-z]* ~/
 echo "Dotfiles linked"
+echo ""
 
 # prompt to install apt packages
 echo "Time to intall stuff: "
 echo "sudo add-apt-repository ppa:neovim-ppa/unstable" # nvim 0.7+ required. Change to 'stable' when ready
-echo "sudo apt update"
-echo "sudo apt install neovim curl ripgrep sqlite3 libsqlite3-dev pip python-neovim python3-neovim"
-
-# vim-plug
-echo "sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'"
+echo "sudo apt update && sudo apt install neovim curl ripgrep sqlite3 libsqlite3-dev pip python-neovim python3-neovim"
--- nvim/plugins.vim
+++ nvim/plugins.vim
@@ -1,3 +1,10 @@
+" Ensure vim-plug is installed
+let data_dir = has('nvim') ? stdpath('data') . '/site' : '~/.vim'
+if empty(glob(data_dir . '/autoload/plug.vim'))
+    silent execute '!curl -fLo '.data_dir.'/autoload/plug.vim --create-dirs  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+    autocmd VimEnter * PlugInstall --sync | source $MYVIMRC
+endif
+
 " plugins
 call plug#begin('~/.config/nvim/vim-plugged')
     Plug 'nvim-lua/popup.nvim' " allows overlays
