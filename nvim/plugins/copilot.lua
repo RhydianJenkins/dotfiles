5318ab2041a11de6de7e3b3a2a71375a7755efae
Rhydian Jenkins <rhydz@msn.com>
2022-06-30 08:56:43 +0100

    Copilot settings

--- nvim/plugins/copilot.lua
+++ nvim/plugins/copilot.lua
@@ -1,9 +1,12 @@
+local opts = { noremap = true, silent = true }
+
 vim.g.copilot_no_tab_map = true -- don't use tab to accept copilot suggestion
 vim.g.copilot_assume_mapped = true
 vim.g.copilot_filetypes = {
     ["*"] = false,
-    -- ["javascript"] = true,
-    -- ["typescript"] = true,
+    ["javascript"] = true,
+    ["typescript"] = true,
+    ["php"] = true,
     -- ["lua"] = true,
     -- ["rust"] = true,
     -- ["c"] = true,
@@ -11,6 +14,9 @@ vim.g.copilot_filetypes = {
     -- ["c++"] = true,
     -- ["go"] = true,
     -- ["python"] = true,
-    -- ["php"] = true,
 }
 vim.api.nvim_set_keymap("i", "<A-c>", 'copilot#Accept("")', { expr = true, silent = true })
+
+-- Copilot
+vim.api.nvim_set_keymap('n', '<leader>cpe', '<cmd>Copilot enable<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader>cpd', '<cmd>Copilot disable<CR>', opts)
