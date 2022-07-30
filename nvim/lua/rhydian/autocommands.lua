1b3376184d361e863e9bdf78b126eac7416c85cd
Rhydian Jenkins <rhydz@msn.com>
2022-07-30 18:22:47 +0100

    Removed redundant whitespace au

--- nvim/lua/rhydian/autocommands.lua
+++ nvim/lua/rhydian/autocommands.lua
@@ -3,9 +3,3 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     command = "silent! lua vim.highlight.on_yank()",
     group = vim.api.nvim_create_augroup("YankHighlight", {}),
 })
-
--- Go to last loc when opening a buffer
-vim.api.nvim_create_autocmd("BufReadPost", {
-    command = [[if line("'\"") > 1 && line("'\"") <= line("$") | execute "normal! g`\"" | endif]],
-    group = vim.api.nvim_create_augroup("GoToLast", {}),
-})
