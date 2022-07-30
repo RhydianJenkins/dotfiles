76a099ba53b437bd7af571b5b66d2c40dfead857
Rhydian Jenkins <rhydz@msn.com>
2022-07-30 18:13:25 +0100

    Autocommands reformat

--- nvim/lua/rhydian/autocommands.lua
+++ nvim/lua/rhydian/autocommands.lua
@@ -1,17 +1,11 @@
 -- Highlight on yank
-vim.api.nvim_create_autocmd(
-    "TextYankPost",
-    {
+vim.api.nvim_create_autocmd("TextYankPost", {
     command = "silent! lua vim.highlight.on_yank()",
-    group = vim.api.nvim_create_augroup("YankHighlight", { clear = true }),
-}
-)
+    group = vim.api.nvim_create_augroup("YankHighlight", {}),
+})
 
--- go to last loc when opening a buffer
-vim.api.nvim_create_autocmd(
-    "BufReadPost",
-    {
+-- Go to last loc when opening a buffer
+vim.api.nvim_create_autocmd("BufReadPost", {
     command = [[if line("'\"") > 1 && line("'\"") <= line("$") | execute "normal! g`\"" | endif]],
-    group = vim.api.nvim_create_augroup("GotoLast", { clear = true }),
-}
-)
+    group = vim.api.nvim_create_augroup("GoToLast", {}),
+})
