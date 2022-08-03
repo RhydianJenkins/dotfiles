bc36344392837f7d493abbca27fab1b8902f1f6d
Rhydian Jenkins <rhydz@msn.com>
2022-08-03 17:47:53 +0100

    More consistent syntax used for keymapping

--- nvim/lua/rhydian/keybinds.lua
+++ nvim/lua/rhydian/keybinds.lua
@@ -52,7 +52,7 @@ vim.api.nvim_set_keymap('n', ']d', '<cmd>Lspsaga diagnostic_jump_next<CR>', opts
 vim.api.nvim_set_keymap('n', '<leader>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
 
 -- LSP
-vim.keymap.set('n', '<leader>a', '<cmd>Lspsaga code_action<CR>', opts)
-vim.keymap.set('n', '<leader>rn', '<cmd>Lspsaga rename<CR>', opts)
-vim.keymap.set('n', 'gD', '<cmd>Lspsaga preview_definition<CR>', opts)
-vim.keymap.set('n', 'K', '<cmd>Lspsaga hover_doc<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader>a', '<cmd>Lspsaga code_action<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader>rn', '<cmd>Lspsaga rename<CR>', opts)
+vim.api.nvim_set_keymap('n', 'gD', '<cmd>Lspsaga preview_definition<CR>', opts)
+vim.api.nvim_set_keymap('n', 'K', '<cmd>Lspsaga hover_doc<CR>', opts)
