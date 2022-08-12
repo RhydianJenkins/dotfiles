b9e4112a7c85112a0e31c6f80923c34229eb53ab
Rhydian Jenkins <rhydz@msn.com>
2022-08-12 10:49:15 +0100

    Checking dap-go plugin exists

--- nvim/lua/rhydian/configs/dap/go.lua
+++ nvim/lua/rhydian/configs/dap/go.lua
@@ -1,1 +1,7 @@
-require('dap-go').setup()
+local status, dap_go = pcall(require, "dap-go")
+if (not status) then
+    print('dap go debug adapter plugin not found')
+    return
+end
+
+dap_go.setup()
