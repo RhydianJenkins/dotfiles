63312fe2a533f40c7de1219f88ebd0d3d432e826
Rhydian Jenkins <rhydz@msn.com>
2022-09-11 11:25:14 +0100

    Peek definition used in place of deprecated preview definition (lspsaga keybind)

--- nvim/lua/rhydian/keybinds.lua
+++ nvim/lua/rhydian/keybinds.lua
@@ -55,7 +55,7 @@ vim.api.nvim_set_keymap('n', '<leader>q', '<cmd>lua vim.diagnostic.setloclist()<
 -- LSP
 vim.api.nvim_set_keymap('n', '<leader>a', '<cmd>Lspsaga code_action<CR>', opts)
 vim.api.nvim_set_keymap('n', '<leader>rn', '<cmd>Lspsaga rename<CR>', opts)
-vim.api.nvim_set_keymap('n', 'K', '<cmd>Lspsaga preview_definition<CR>', opts)
+vim.api.nvim_set_keymap('n', 'K', '<cmd>Lspsaga peek_definition<CR>', opts)
 vim.api.nvim_set_keymap('n', '<C-k>', '<cmd>Lspsaga hover_doc<CR>', opts)
 
 -- DAP
