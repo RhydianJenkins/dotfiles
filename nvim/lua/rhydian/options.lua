6db1712fe4ba82f341cc493a79f8797e9b358d38
Rhydian Jenkins <rhydz@msn.com>
2022-11-14 20:34:27 +0000

    tabstop increased to 8

--- nvim/lua/rhydian/options.lua
+++ nvim/lua/rhydian/options.lua
@@ -1,7 +1,7 @@
 vim.opt.nu = true
 vim.opt.relativenumber = false
 vim.opt.errorbells = false
-vim.opt.tabstop = 4
+vim.opt.tabstop = 8
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
