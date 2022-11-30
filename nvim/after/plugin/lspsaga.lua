7f50acf3fec7af0fb1f0b8d75552a085f6a13f53
Rhydian Jenkins <rhydz@msn.com>
2022-11-30 23:24:40 +0000

    Lspsaga reverted, unused plugins gone

--- nvim/after/plugin/lspsaga.lua
+++ nvim/after/plugin/lspsaga.lua
@@ -1,17 +1,25 @@
 local present, saga = pcall(require, "lspsaga")
 
 if not present then
-    print("saga plugin not found")
+    print("lspsaga plugin not found")
     return
 end
 
 saga.init_lsp_saga({
     border_style = "rounded",
+    preview_lines_above = 5,
+    finder_request_timeout = 3000,
     code_action_lightbulb = {
         enable = false,
     },
-    code_action_keys = {
-        quit = "<C-c>",
-        exec = "<CR>",
+    finder_action_keys = {
+        quit = {'q', '<ESC>', '<C-c>'},
+        vsplit = 'v',
+        split = 's',
+    },
+    finder_action_keys = {
+        quit = {'q', '<ESC>', '<C-c>'},
+        vsplit = 'v',
+        split = 's',
     },
 })
--- nvim/after/plugin/mason.lua
+++ nvim/after/plugin/mason.lua
@@ -1,9 +1,16 @@
 local mason_present, mason = pcall(require, "mason")
+local neodev_present, neodev = pcall(require, "neodev")
 local lspconfig_present, lspconfig = pcall(require, "lspconfig")
 local mason_lspconfig_present, mason_lspconfig = pcall(require, "mason-lspconfig")
+local cmp_nvim_lsp_present, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
 
 if not mason_present then
     print("mason plugin not found")
+    return
+end
+
+if not neodev_present then
+    print("neodev plugin not found")
     return
 end
 
@@ -12,17 +19,25 @@ if not lspconfig_present then
     return
 end
 
+if not cmp_nvim_lsp_present then
+    print("cmp_nvim_lsp plugin not found")
+    return
+end
+
 if not mason_lspconfig_present then
     print("mason lspconfig plugin not found")
     return
 end
+
 local lsp_defaults = lspconfig.util.default_config
 
 lsp_defaults.capabilities = vim.tbl_deep_extend(
     'force',
     lsp_defaults.capabilities,
-    require('cmp_nvim_lsp').default_capabilities()
+    cmp_nvim_lsp.default_capabilities()
 )
+
+neodev.setup({})
 
 mason.setup({
     ui = {
@@ -36,6 +51,8 @@ mason.setup({
 
 mason_lspconfig.setup({
     ensure_installed = {
+        'phpactor',
+        'intellephense',
         'tsserver',
         'eslint',
         'html',
@@ -45,15 +62,8 @@ mason_lspconfig.setup({
 
 mason_lspconfig.setup_handlers({
     function(server)
-        lspconfig[server].setup({})
-    end,
-    ['tsserver'] = function()
-        lspconfig.tsserver.setup({
-            settings = {
-                completions = {
-                    completeFunctionCalls = true
-                }
-            }
+        lspconfig[server].setup({
+            capabilities = capabilities,
         })
-    end
+    end,
 })
