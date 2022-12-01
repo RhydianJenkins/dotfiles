960635d34de97e13a6ac9a610c2f393148f0043d
Rhydian Jenkins <rhydz@msn.com>
2022-12-01 11:18:34 +0000

    Rename file, removed vsnip

--- nvim/after/plugin/cmp.lua
+++ nvim/after/plugin/cmp.lua
@@ -17,11 +17,6 @@ local has_words_before = function()
     return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
--- Send feed keys with special codes (used by <S-TAB> mapping)
-local feedkey = function(key, mode)
-    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
-end
-
 -- For cmp formatting
 local source_mapping = {
     buffer = "[Buffer]",
@@ -33,11 +28,6 @@ local source_mapping = {
 }
 
 cmp.setup({
-    snippet = {
-        expand = function(args)
-            vim.fn["vsnip#anonymous"](args.body)
-        end,
-    },
     window = {
         completion = cmp.config.window.bordered(),
         documentation = cmp.config.window.bordered(),
@@ -49,8 +39,6 @@ cmp.setup({
         ['<Tab>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_next_item()
-            elseif vim.fn["vsnip#available"](1) == 1 then
-                feedkey("<Plug>(vsnip-expand-or-jump)", "")
             elseif has_words_before() then
                 cmp.complete()
             else
@@ -60,15 +48,12 @@ cmp.setup({
         ['<S-Tab>'] = cmp.mapping(function()
             if cmp.visible() then
                 cmp.select_prev_item()
-            elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-                feedkey("<Plug>(vsnip-jump-prev)", "")
             end
         end, { "i", "s" }),
     }),
     sources = cmp.config.sources({
         { name = 'nvim_lsp' },
         { name = 'nvim_lua' },
-        { name = 'vsnip' },
         { name = 'copilot' },
         { name = 'cmp_tabnine' },
         { name = 'path' },
