6f9fe4f5c8a1b80d58051adf5019c6cb027f5af8
Rhydian Jenkins <rhydz@msn.com>
2022-12-08 11:58:41 +0000

    codespell added to null ls diagnostcs

--- nvim/after/plugin/nullls.lua
+++ nvim/after/plugin/nullls.lua
@@ -8,6 +8,6 @@ end
 null_ls.setup({
     sources = {
         null_ls.builtins.formatting.stylua,
-        -- null_ls.builtins.completion.spell,
+        null_ls.builtins.diagnostics.codespell,
     },
 })
