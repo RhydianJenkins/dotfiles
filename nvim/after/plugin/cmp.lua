032ddca2b9bfb3762ad4ca0640c689dca3cb56da
Rhydian Jenkins <rhydz@msn.com>
2022-12-12 21:17:15 +0000

    Some cmp refactoring

--- nvim/after/plugin/cmp.lua
+++ nvim/after/plugin/cmp.lua
@@ -11,7 +11,6 @@ if not lspkind_present then
     return
 end
 
--- Check if there's a word before the cursor (used by <TAB> mapping)
 local has_words_before = function()
     local line, col = unpack(vim.api.nvim_win_get_cursor(0))
     return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
@@ -47,23 +46,31 @@ cmp.setup({
     },
     mapping = cmp.mapping.preset.insert({
         ["<C-Space>"] = cmp.mapping.complete(),
+
         ["<C-e>"] = cmp.mapping.abort(),
+
         ["<CR>"] = cmp.mapping.confirm({ select = false }), -- set select to true to also confirm even if not explicitly selected
+
         ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_next_item()
+            elseif vim.fn["vsnip#available"](1) == 1 then
+                feedkey("<Plug>(vsnip-expand-or-jump)", "")
             elseif has_words_before() then
                 cmp.complete()
             else
-                -- fall back to the default behavior
                 fallback()
             end
         end, { "i", "s" }),
+
         ["<S-Tab>"] = cmp.mapping(function()
             if cmp.visible() then
                 cmp.select_prev_item()
+            elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+                feedkey("<Plug>(vsnip-jump-prev)", "")
             end
         end, { "i", "s" }),
+
         ["<C-n>"] = cmp.mapping(function()
             if vim.fn["vsnip#available"](1) == 1 then
                 feedkey("<Plug>(vsnip-expand-or-jump)", "")
@@ -139,3 +146,4 @@ cmp.setup.cmdline(":", {
         { name = "cmdline" },
     }),
 })
+
