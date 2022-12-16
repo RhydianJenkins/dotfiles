ea35b43582499482e186d50c25f584745d1d388a
Rhydian Jenkins <rhydz@msn.com>
2022-12-16 19:15:45 +0000

    Treesitter config tweaks

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,5 +1,6 @@
 require("nvim-treesitter.configs").setup({
-    ensure_installed = { "php", "javascript", "typescript", "lua" },
+    ensure_installed = { "php", "javascript", "typescript", "lua", "rust", "help" },
+    auto_install = true,
     highlight = {
         enable = true,
 
@@ -7,12 +8,12 @@ require("nvim-treesitter.configs").setup({
         -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
         -- Using this option may slow down your editor, and you may see some duplicate highlights.
         -- Instead of true it can also be a list of languages
-        additional_vim_regex_highlighting = true,
+        additional_vim_regex_highlighting = false,
     },
     rainbow = {
         enable = true,
         extended_mode = true, -- also highlight non-bracket delimiters
-        max_file_lines = nil, -- do not enable for files with more than n lines
+        max_file_lines = 5000, -- do not enable for files with more than n lines
         -- disable = { "jsx", "cmp" } -- list of languages you want to ignore
         -- colors = {} -- table of hex colours
         -- termcoors = {} -- table of color name strings
