a3a29bf08c5734c9ceef231ed637f74587af7057
Rhydian Jenkins <rhydz@msn.com>
2022-12-27 22:40:05 +0000

    Lua formatter for neovim printing

--- nvim/after/plugin/printer.lua
+++ nvim/after/plugin/printer.lua
@@ -9,6 +9,11 @@ printer.setup({
     keymap = "yP",
     behavior = "yank",
     yank_register = "+",
+    formatters = {
+        lua = function(text_inside, text_var)
+            return string.format("print([[%s: ]] .. vim.inspect(%s))", text_inside, text_var)
+        end,
+    },
 })
 
 vim.keymap.set("n", "yp", "<Plug>(printer_print)iw", { desc = "[Y]ank [p]rint" })
