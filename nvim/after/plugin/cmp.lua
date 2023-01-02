7139cc21eb975fd15cd43f4935bc9916e65f26ca
Rhydian Jenkins <RhydianJenkins@users.noreply.github.com>
2023-01-02 17:58:12 +0000

    Luacheck added to workflow (#13)

--- nvim/after/plugin/cmp.lua
+++ nvim/after/plugin/cmp.lua
@@ -17,11 +17,6 @@ if not luasnip_present then
     return
 end
 
-local has_words_before = function()
-    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
 require("luasnip.loaders.from_vscode").lazy_load()
 
 vim.api.nvim_set_hl(0, "CmpItemKindCopilot", { fg = "#6CC644" })
@@ -53,15 +48,14 @@ cmp.setup({
 
         ["<C-e>"] = cmp.mapping.abort(),
 
-        ["<CR>"] = cmp.mapping.confirm({ select = false }), -- set select to true to also confirm even if not explicitly selected
+        -- set select to true to also confirm even if not explicitly selected
+        ["<CR>"] = cmp.mapping.confirm({ select = false }),
 
         ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_next_item()
             elseif luasnip.expand_or_jumpable() then
                 luasnip.expand_or_jump()
-            elseif has_words_before() then
-                cmp.complete()
             else
                 fallback()
             end
