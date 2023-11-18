d521ea56dd275506f44115d78ed5d01da77805d0
Rhydian Jenkins <rhydz@msn.com>
2023-11-18 19:43:55 +0000

    Clean up nvimtree config

--- nvim/lua/rhydian/packer.lua
+++ nvim/lua/rhydian/packer.lua
@@ -22,9 +22,6 @@ return require("packer").startup(function(use)
 
     use({
         "nvim-tree/nvim-tree.lua",
-        config = function()
-            require("nvim-tree").setup({})
-        end,
         requires = {
             "nvim-tree/nvim-web-devicons",
         },
