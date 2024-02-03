42925c854e83502f66ef54e1bff080d5b635fb7e
Rhydz@msn.com <Rhydz@msn.com>
2024-02-03 14:20:10 +0000

    Be more specific with class_dirs for symfony util navigation

--- nvim/after/plugin/symfonyutils.lua
+++ nvim/after/plugin/symfonyutils.lua
@@ -7,7 +7,19 @@ end
 
 symfony_utils.setup({
     class_dirs = {
-        "connect",
+        "connect/app/bookings/src",
+        "connect/app/bookings/tests",
+        "connect/app/bookings-checkout/src",
+        "connect/app/bookings-checkout/tests",
+        "connect/app/customer-manager/src",
+        "connect/app/customer-manager/tests",
+        "connect/app/dashboard/src",
+        "connect/app/dashboard/tests",
+        "connect/app/integrations/src",
+        "connect/app/integrations/src",
+        "connect/app/webhooks/src",
+        "connect/app/webhooks/tests",
+        "connect/src",
         "library/BaseKit",
         "plugins/ecommerce/src",
         "control/src",
