8203c2ffb2a5042e0a8b7bb957ff49b7f71335c2
Rhydian Jenkins <rhydz@msn.com>
2024-02-11 12:02:40 +0000

    Use harpoon2 branch, with telescope picker

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -1,29 +1,49 @@
-local present, harpoon = pcall(require, "harpoon")
+local telescope_present, telescope_config = pcall(require, "telescope.config")
+local harpoon_present, harpoon = pcall(require, "harpoon")
 
-if not present then
-    print("harpoon plugin not found")
+if not telescope_present then
+    print("Telescope plugin required for harpoon")
     return
 end
 
----@param mode "n" | "i" | "v"
----@param key string
----@param operation string
----@param desc string
-local function map(mode, key, operation, desc)
-    local opts = { noremap = true, silent = true, desc = desc }
-    vim.api.nvim_set_keymap(mode, key, operation, opts)
+if not harpoon_present then
+    print("Harpoon plugin not found")
+    return
 end
 
-map("n", "<leader>m", '<cmd>lua require("harpoon.mark").add_file()<CR>', "Add mark")
-map("n", "<leader>h", '<cmd>lua require("harpoon.ui").toggle_quick_menu()<CR>', "Toggle quick menu")
-map("n", "<leader>1", '<cmd>lua require("harpoon.ui").nav_file(1)<CR>', "Navigate to mark 1")
-map("n", "<leader>2", '<cmd>lua require("harpoon.ui").nav_file(2)<CR>', "Navigate to mark 2")
-map("n", "<leader>3", '<cmd>lua require("harpoon.ui").nav_file(3)<CR>', "Navigate to mark 3")
-map("n", "<leader>4", '<cmd>lua require("harpoon.ui").nav_file(4)<CR>', "Navigate to mark 4")
-map("n", "<leader>5", '<cmd>lua require("harpoon.ui").nav_file(5)<CR>', "Navigate to mark 5")
+---@param key string
+---@param operation string|function
+---@param desc string
+local function map(key, operation, desc)
+    local opts = { noremap = true, silent = true, desc = desc }
+    vim.keymap.set("n", key, operation, opts)
+end
 
-harpoon.setup({
-    menu = {
-        width = vim.api.nvim_win_get_width(0) - 5,
-    },
-})
+
+---@param harpoon_files table
+local function toggle_telescope(harpoon_files)
+    local file_paths = {}
+    for _, item in ipairs(harpoon_files.items) do
+        table.insert(file_paths, item.value)
+    end
+
+    require("telescope.pickers").new({}, {
+        prompt_title = "Harpoon",
+        finder = require("telescope.finders").new_table({
+            results = file_paths,
+        }),
+        previewer = telescope_config.file_previewer({}),
+        sorter = telescope_config.generic_sorter({}),
+    }):find()
+end
+
+
+harpoon.setup({})
+
+map("<leader>m", function() harpoon:list():append() end, "Add mark")
+map("<leader>h", function() toggle_telescope(harpoon:list()) end, "Toggle quick menu")
+map("<leader>1", function() harpoon:list():select(1) end, "Navigate to mark 1")
+map("<leader>2", function() harpoon:list():select(2) end, "Navigate to mark 2")
+map("<leader>3", function() harpoon:list():select(3) end, "Navigate to mark 3")
+map("<leader>4", function() harpoon:list():select(4) end, "Navigate to mark 4")
+map("<leader>5", function() harpoon:list():select(5) end, "Navigate to mark 5")
