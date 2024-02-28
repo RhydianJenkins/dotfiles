a17de0ca419e57ae0ed91054a94389c15163d3b8
Rhydz@msn.com <Rhydz@msn.com>
2024-02-28 09:24:53 +0000

    Disable wezterm update checking

--- dotfiles/.wezterm.lua
+++ dotfiles/.wezterm.lua
@@ -5,6 +5,7 @@ if wezterm.config_builder then
     config = wezterm.config_builder()
 end
 
+config.check_for_updates = false
 config.color_scheme = "tokyonight"
 config.enable_tab_bar = false
 config.window_padding = {
