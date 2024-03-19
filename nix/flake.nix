e417d102c1328a3328b8fbb683f23b123bd328e1
Rhydz@msn.com <Rhydz@msn.com>
2024-03-19 17:30:24 +0000

    Generate my first lock file

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,9 +1,19 @@
 {
-  description = "Rhydian's system configuration";
+    description = "Rhydian's configuration flake";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
+    inputs = {
+        nixpkgs = {
+            url = "github:NixOS/nixpkgs/nixos-20.03";
+        };
+    };
 
-  outputs = { self, nixpkgs }: {};
+    outputs = { self, nixpkgs }: {
+        defaultPackage.x86_64-linux = with import nixpkgs { system = "x86_64-linux"; };
+        stdenv.mkDerivation {
+            name = "hello";
+            src = self;
+            buildPhase = "gcc -o hello ./hello.c";
+            installPhase = "mkdir -p $out/bin; install -t $out/bin hello";
+        };
+    };
 }
