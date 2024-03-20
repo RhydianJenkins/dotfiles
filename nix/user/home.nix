c9f6f09125374dc2f3a4d50f7df47ad7842c76e3
Rhydian Jenkins <rhydz@msn.com>
2024-03-20 19:41:29 +0000

    Import dotfiles with home-manager

--- nix/user/home.nix
+++ nix/user/home.nix
@@ -10,16 +10,16 @@ in {
     ];
   };
 
-  programs = {
-      zsh = {
-          enable = true;
-          shellAliases = {
-              test = "echo 'it worked'";
-              c = "clear";
-              ll = "ls -la";
-          };
-      };
-  };
+  # Install existing dotfiles
+  # These will evenaually be added to the nix home-manager declaration
+  home.file.".aliases".source = ../../dotfiles/.aliases;
+  home.file.".bashrc".source = ../../dotfiles/.bashrc;
+  home.file.".functions".source = ../../dotfiles/.functions;
+  home.file.".inputrc".source = ../../dotfiles/.inputrc;
+  home.file.".tmux.conf".source = ../../dotfiles/.tmux.conf;
+  home.file.".tmuxifier".source = ../../dotfiles/.tmuxifier;
+  home.file.".wezterm.lua".source = ../../dotfiles/.wezterm.lua;
+  home.file.".zshrc".source = ../../dotfiles/.zshrc;
 
-  # services.gpg-agent.enable = true;
+  services.gpg-agent.enable = true;
 }
