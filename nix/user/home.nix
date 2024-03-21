9abd708f61aca34a88a5735521e1ec96e2cdfc6d
Rhydian Jenkins <rhydz@msn.com>
2024-03-21 19:09:10 +0000

    Keep home-manager after switching

--- nix/user/home.nix
+++ nix/user/home.nix
@@ -1,25 +1,28 @@
 {pkgs, ...}: let
-  username = "rhydian";
+username = "rhydian";
 in {
-  home = {
-    username = "${username}";
-    homeDirectory = "/home/${username}";
-    stateVersion = "23.11";
+    home = {
+        username = "${username}";
+        homeDirectory = "/home/${username}";
+        stateVersion = "23.11";
 
-    packages = with pkgs; [
-    ];
-  };
+        packages = with pkgs; [
+        ];
+    };
 
-  # Install existing dotfiles
-  # These will evenaually be added to the nix home-manager declaration
-  home.file.".aliases".source = ../../dotfiles/.aliases;
-  home.file.".bashrc".source = ../../dotfiles/.bashrc;
-  home.file.".functions".source = ../../dotfiles/.functions;
-  home.file.".inputrc".source = ../../dotfiles/.inputrc;
-  home.file.".tmux.conf".source = ../../dotfiles/.tmux.conf;
-  home.file.".tmuxifier".source = ../../dotfiles/.tmuxifier;
-  home.file.".wezterm.lua".source = ../../dotfiles/.wezterm.lua;
-  home.file.".zshrc".source = ../../dotfiles/.zshrc;
+    # Install existing dotfiles
+    # These will evenaually be added to the nix home-manager declaration
+    home.file.".aliases".source = ../../dotfiles/.aliases;
+    home.file.".bashrc".source = ../../dotfiles/.bashrc;
+    home.file.".functions".source = ../../dotfiles/.functions;
+    home.file.".inputrc".source = ../../dotfiles/.inputrc;
+    home.file.".tmux.conf".source = ../../dotfiles/.tmux.conf;
+    home.file.".tmuxifier".source = ../../dotfiles/.tmuxifier;
+    home.file.".wezterm.lua".source = ../../dotfiles/.wezterm.lua;
+    home.file.".zshrc".source = ../../dotfiles/.zshrc;
+    # home.file.".config/i3".source = ../../i3;
 
-  services.gpg-agent.enable = true;
+    services.gpg-agent.enable = true;
+
+    programs.home-manager.enable = true;
 }
