23e9e908f2f9a9a1edfccf6591a90eccfa5f6a83
Rhydian Jenkins <rhydz@msn.com>
2024-03-21 20:04:52 +0000

    formatting and nixosrebuild alias

--- nix/system/configuration.nix
+++ nix/system/configuration.nix
@@ -5,197 +5,187 @@
 { config, pkgs, ... }:
 
 {
-  imports = [
-    ./hardware-configuration.nix
-  ];
-
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  nix = {
-    settings = {
-      warn-dirty = false;
-      experimental-features = "nix-command flakes";
-      auto-optimise-store = true;
-    };
-  };
-
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Set your time zone.
-  time.timeZone = "Europe/London";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_GB.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_GB.UTF-8";
-    LC_IDENTIFICATION = "en_GB.UTF-8";
-    LC_MEASUREMENT = "en_GB.UTF-8";
-    LC_MONETARY = "en_GB.UTF-8";
-    LC_NAME = "en_GB.UTF-8";
-    LC_NUMERIC = "en_GB.UTF-8";
-    LC_PAPER = "en_GB.UTF-8";
-    LC_TELEPHONE = "en_GB.UTF-8";
-    LC_TIME = "en_GB.UTF-8";
-  };
-
-  services.xserver = {
-    enable = true;
-
-    desktopManager = {
-      xterm.enable = false;
-    };
-
-    displayManager = {
-      defaultSession = "none+i3";
-    };
-
-    windowManager.i3 = {
-      enable = true;
-      extraPackages = with pkgs; [
-        dmenu
-        i3status
-        i3lock
-        i3blocks
-      ];
-    };
-
-    xkb = {
-      layout = "gb";
-      variant = "";
-    };
-  };
-
-  # Configure console keymap
-  console.keyMap = "uk";
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  programs.zsh.enable = true;
-
-  users.users.rhydian = {
-    isNormalUser = true;
-    description = "Rhydian";
-    extraGroups = [ "networkmanager" "wheel" ];
-    shell = pkgs.zsh;
-  };
-
-  # Enable automatic login for the user.
-  services.xserver.displayManager.autoLogin.enable = true;
-  services.xserver.displayManager.autoLogin.user = "rhydian";
-
-  # Set keyboard refresh rate
-  services.xserver.autoRepeatDelay = 200;
-  services.xserver.autoRepeatInterval = 40;
-
-  # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
-  systemd.services."getty@tty1".enable = false;
-  systemd.services."autovt@tty1".enable = false;
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  environment.pathsToLink = [ "/libexec" ];
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    wget
-    curl
-    git
-    xclip
-    gcc
-    unzip
-    python3
-    nodejs_21
-    wezterm
-    firefox
-    rofi
-    neovim
-    ripgrep
-    tldr
-    cargo
-    lazygit
-    neofetch
-    haskellPackages.greenclip
-  ];
-
-  fonts = {
-    packages = with pkgs; [
-      noto-fonts
-      noto-fonts-cjk
-      noto-fonts-emoji
-      font-awesome
-      source-han-sans
-      source-han-sans-japanese
-      source-han-serif-japanese
-      (nerdfonts.override {fonts = ["Meslo"];})
+    imports = [
+        ./hardware-configuration.nix
     ];
-    fontconfig = {
-      enable = true;
-      defaultFonts = {
-        monospace = ["Meslo LG M Regular Nerd Font Complete Mono"];
-        serif = ["Noto Serif" "Source Han Serif"];
-        sansSerif = ["Noto Sans" "Source Han Sans"];
-      };
+
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
+
+    nix = {
+        settings = {
+            warn-dirty = false;
+            experimental-features = "nix-command flakes";
+            auto-optimise-store = true;
+        };
     };
-  };
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+    networking.hostName = "nixos"; # Define your hostname.
+    networking.networkmanager.enable = true;
+    time.timeZone = "Europe/London";
+    i18n.defaultLocale = "en_GB.UTF-8";
+    i18n.extraLocaleSettings = {
+        LC_ADDRESS = "en_GB.UTF-8";
+        LC_IDENTIFICATION = "en_GB.UTF-8";
+        LC_MEASUREMENT = "en_GB.UTF-8";
+        LC_MONETARY = "en_GB.UTF-8";
+        LC_NAME = "en_GB.UTF-8";
+        LC_NUMERIC = "en_GB.UTF-8";
+        LC_PAPER = "en_GB.UTF-8";
+        LC_TELEPHONE = "en_GB.UTF-8";
+        LC_TIME = "en_GB.UTF-8";
+    };
 
-  # List services that you want to enable:
+    services.xserver = {
+        enable = true;
+        autorun = true;
 
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
+        desktopManager = {
+            xterm.enable = false;
+        };
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+        displayManager = {
+            defaultSession = "none+i3";
+            lightdm.enable = true;
+        };
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.11"; # Did you read the comment?
+        windowManager.i3 = {
+            enable = true;
+            extraPackages = with pkgs; [
+                dmenu
+                i3status
+                i3lock
+                i3blocks
+            ];
+        };
+
+        xkb = {
+            layout = "gb";
+            variant = "";
+        };
+    };
+
+# Configure console keymap
+    console.keyMap = "uk";
+
+# Enable CUPS to print documents.
+    services.printing.enable = true;
+
+# Enable sound with pipewire.
+    sound.enable = true;
+    hardware.pulseaudio.enable = false;
+    security.rtkit.enable = true;
+    services.pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+# If you want to use JACK applications, uncomment this
+#jack.enable = true;
+
+# use the example session manager (no others are packaged yet so this is enabled by default,
+# no need to redefine it in your config for now)
+#media-session.enable = true;
+    };
+
+# Enable touchpad support (enabled default in most desktopManager).
+# services.xserver.libinput.enable = true;
+
+    programs.zsh.enable = true;
+
+    users.users.rhydian = {
+        isNormalUser = true;
+        description = "Rhydian";
+        extraGroups = [ "networkmanager" "wheel" ];
+        shell = pkgs.zsh;
+    };
+
+# Enable automatic login for the user.
+    services.xserver.displayManager.autoLogin.enable = true;
+    services.xserver.displayManager.autoLogin.user = "rhydian";
+
+# Set keyboard refresh rate
+    services.xserver.autoRepeatDelay = 200;
+    services.xserver.autoRepeatInterval = 40;
+
+# Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
+    systemd.services."getty@tty1".enable = false;
+    systemd.services."autovt@tty1".enable = false;
+
+# Allow unfree packages
+    nixpkgs.config.allowUnfree = true;
+
+    environment.pathsToLink = [ "/libexec" ];
+
+# List packages installed in system profile. To search, run:
+# $ nix search wget
+    environment.systemPackages = with pkgs; [
+        wget
+            curl
+            git
+            xclip
+            gcc
+            unzip
+            python3
+            nodejs_21
+            wezterm
+            firefox
+            rofi
+            neovim
+            ripgrep
+            tldr
+            cargo
+            lazygit
+            neofetch
+            haskellPackages.greenclip
+    ];
+
+    fonts = {
+        packages = with pkgs; [
+            noto-fonts
+                noto-fonts-cjk
+                noto-fonts-emoji
+                font-awesome
+                source-han-sans
+                source-han-sans-japanese
+                source-han-serif-japanese
+                (nerdfonts.override {fonts = ["Meslo"];})
+        ];
+        fontconfig = {
+            enable = true;
+            defaultFonts = {
+                monospace = ["Meslo LG M Regular Nerd Font Complete Mono"];
+                serif = ["Noto Serif" "Source Han Serif"];
+                sansSerif = ["Noto Sans" "Source Han Sans"];
+            };
+        };
+    };
+
+# Some programs need SUID wrappers, can be configured further or are
+# started in user sessions.
+# programs.mtr.enable = true;
+# programs.gnupg.agent = {
+#   enable = true;
+#   enableSSHSupport = true;
+# };
+
+# List services that you want to enable:
+
+# Enable the OpenSSH daemon.
+    services.openssh.enable = true;
+
+# Open ports in the firewall.
+# networking.firewall.allowedTCPPorts = [ ... ];
+# networking.firewall.allowedUDPPorts = [ ... ];
+# Or disable the firewall altogether.
+# networking.firewall.enable = false;
+
+# This value determines the NixOS release from which the default
+# settings for stateful data, like file locations and database versions
+# on your system were taken. It‘s perfectly fine and recommended to leave
+# this value at the release version of the first install of this system.
+# Before changing this value read the documentation for this option
+# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+    system.stateVersion = "23.11"; # Did you read the comment?
 
 }
