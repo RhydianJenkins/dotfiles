6aca450e018f55760f244663587d38bfff88b813
Rhydz@msn.com <Rhydz@msn.com>
2024-04-17 09:55:56 +0100

    Remove unused binding

--- nix/flake.nix
+++ nix/flake.nix
@@ -7,7 +7,7 @@
         };
     };
 
-    outputs = { self, nixpkgs, home-manager, ... }: let
+    outputs = { nixpkgs, home-manager, ... }: let
         system = "x86_64-linux";
         lib = nixpkgs.lib;
         pkgs = nixpkgs.legacyPackages.${system};
