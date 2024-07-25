4737aef834ebc7fa2b7bd0d0fc74a41ec5838e67
Rhydz@msn.com <Rhydz@msn.com>
2024-07-25 11:04:00 +0100

    Remove whichkey custom registers

--- nvim/after/plugin/whichkey.lua
+++ nvim/after/plugin/whichkey.lua
@@ -6,18 +6,3 @@ if not present then
 end
 
 whichkey.setup({})
-
-whichkey.register({
-    ["<leader>"] = {
-        t = { "Telescope/Test" },
-        r = { "Replace/Rename" },
-        w = { "Window controls" },
-        ["<leader>"] = {
-            name = "Swap buffers...",
-            h = { "Swap buffer left" },
-            j = { "Swap buffer down" },
-            k = { "Swap buffer up" },
-            l = { "Swap buffer right" },
-        },
-    },
-})
