2707943cb5b09cf72aaedae1792c9794001d16b8
Rhydz@msn.com <Rhydz@msn.com>
2024-12-13 16:03:19 +0000

    Remove unused region_to_text

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -5,17 +5,6 @@ local theme = "ivy"
 pcall(telescope.load_extension, "fzf")
 pcall(telescope.load_extension, "harpoon")
 
-function region_to_text(region)
-    local text = ""
-    local maxcol = vim.v.maxcol
-    for line, cols in vim.spairs(region) do
-        local endcol = cols[2] == maxcol and -1 or cols[2]
-        local chunk = vim.api.nvim_buf_get_text(0, line, cols[1], line, endcol, {})[1]
-        text = ("%s%s\n"):format(text, chunk)
-    end
-    return text
-end
-
 telescope.setup({
     extensions = {
         fzf = {
