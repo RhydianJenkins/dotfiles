bfd7f44404ff297dfb55e53f020d4981d08a6152
Rhydian Jenkins <rhydz@msn.com>
2024-12-18 23:45:22 +0000

    Improve oil keymaps

--- nvim/after/plugin/oil.lua
+++ nvim/after/plugin/oil.lua
@@ -9,10 +9,20 @@ oil.setup({
     keymaps = {
         ["<C-p>"] = false,
         ["<C-s>"] = false,
+        ["<S-Y>"] = {
+            desc = "Yank the full path to the clipboard",
+            callback = require("oil.actions").copy_entry_path.callback,
+        },
     },
     view_options = {
         show_hidden = true,
     },
 })
 
-vim.keymap.set("n", "<C-b>", oil.toggle_float, { desc = "Toggle file explore at current buffer" })
+vim.keymap.set("n", "<C-b>", function()
+    if vim.bo.filetype == "oil" then
+        oil.close()
+    else
+        oil.open()
+    end
+end, { desc = "Toggle file explore at current buffer" })
