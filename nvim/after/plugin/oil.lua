6eb1238abe767511dd9ee574401055f790c59d77
Rhydz@msn.com <Rhydz@msn.com>
2024-12-18 15:48:25 +0000

    Replace nvim-tree with oil

---
+++ nvim/after/plugin/oil.lua
@@ -0,0 +1,17 @@
+local status, oil = pcall(require, "oil")
+
+if not status then
+    print("oil plugin not found")
+    return
+end
+
+oil.setup({
+    keymaps = {
+        ["<C-p>"] = false, -- false means unset
+    },
+    view_options = {
+        show_hidden = true,
+    },
+})
+
+vim.keymap.set("n", "<C-b>", oil.toggle_float, { desc = "Toggle file explore at current buffer" })
