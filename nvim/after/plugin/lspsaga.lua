69766f19c5c613dd6f994a7d624fb4f2227946ae
Rhydian Jenkins <RhydianJenkins@users.noreply.github.com>
2025-07-10 09:15:18 +0100

    Update nvim/after/plugin/lspsaga.lua
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- nvim/after/plugin/lspsaga.lua
+++ nvim/after/plugin/lspsaga.lua
@@ -33,8 +33,8 @@ saga.setup({
 })
 
 vim.keymap.set("n", "gr", "<cmd>Lspsaga finder<CR>", { desc = "[G]o to [r]eferences" })
-vim.keymap.set("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<CR>", { desc = "Next [d]iagnostic" })
-vim.keymap.set("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>", { desc = "Previous [d]iagnostic" })
+vim.keymap.set("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<CR>", { desc = "Previous [d]iagnostic" })
+vim.keymap.set("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>", { desc = "Next [d]iagnostic" })
 vim.keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", { desc = "[C]ode [a]ction" })
 vim.keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", { desc = "[R]e[n]ame" })
 vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", { desc = "Hover doc" })
