b02f226bdf7480db0ae9d131a0aeb81ae05cda6f
Rhydian Jenkins <rhydz@msn.com>
2025-10-27 21:41:46 +0000

    Remove comments

--- nix/system/modules/common.nix
+++ nix/system/modules/common.nix
@@ -43,39 +43,6 @@
             user = "rhydian";
         };
 
-        # displayManager.autoLogin.enable = true;
-        # displayManager.autoLogin.user = "rhydian";
-        # xserver = {
-        #     enable = true;
-        #     autorun = true;
-        #     autoRepeatDelay = 200;
-        #     autoRepeatInterval = 40;
-        #
-        #     desktopManager = {
-        #         xterm.enable = false;
-        #     };
-        #
-        #     displayManager = {
-        #         lightdm.enable = true;
-        #     };
-        #
-        #     windowManager.i3 = {
-        #         enable = true;
-        #         extraPackages = with pkgs; [
-        #             dmenu
-        #             i3status
-        #             i3lock
-        #             i3blocks
-        #         ];
-        #     };
-        #
-        #     xkb = {
-        #         layout = "gb";
-        #         variant = "";
-        #         options = "caps:escape";
-        #     };
-        # };
-
         pipewire = {
             enable = true;
             alsa.enable = true;
