fb4828322e6cd91b01d1c9d47b295efb8b826638
Rhydian Jenkins <rhydz@msn.com>
2025-12-18 12:56:11 +0000

    Remove power management

--- nix/system/machines/home-desktop/nvidia.nix
+++ nix/system/machines/home-desktop/nvidia.nix
@@ -7,7 +7,6 @@
 
   hardware.nvidia = {
     modesetting.enable = true;
-    powerManagement.enable = true;
     open = true;
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
