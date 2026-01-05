107159163e8fb3eafc43956209f759adb13c2736
Rhydian Jenkins <rhydz@mens.com>
2026-01-05 16:44:40 +0000

    Update stoplight package to also have desktop and icon in final out

--- nix/home/customPkgs/stoplight.nix
+++ nix/home/customPkgs/stoplight.nix
@@ -1,24 +1,39 @@
-{ pkgs, stdenv, fetchurl, appimageTools }: let
+{
+    appimageTools,
+    fetchurl,
+    lib,
+    stdenv,
+}: let
     pname = "stoplight-studio";
-    version = "2.10.0";
+    version = "2.10.0-stable";
 
     src = fetchurl {
-        url = "https://github.com/stoplightio/studio/releases/download/v${version}-stable.9587.git-0533c10/stoplight-studio-linux-x86_64.AppImage";
-        sha256 = "pCUosO1DpGsJFc1CnkFoJDuYZzmJDSfBtbtmEABGpYU="; # You'll need to get the correct hash
+        url = "https://github.com/stoplightio/studio/releases/download/v${version}.9587.git-0533c10/stoplight-studio-linux-x86_64.AppImage";
+        sha256 = "11d58q010rmvnp0jf3c975krhfr4d10rwhnd2l4np923xnq2h9d4";
     };
+
+    appimageContents = appimageTools.extract { inherit pname version src; };
 in
-    appimageTools.wrapType2 {
-        inherit pname version src;
+appimageTools.wrapType2 {
+    inherit pname version src;
 
-        extraInstallCommands = ''
-mv $out/bin/${pname} $out/bin/stoplight
-        '';
+    extraPkgs = pkgs: with pkgs; [
+        gtk2
+    ];
 
-        meta = with pkgs.lib; {
-            description = "Stoplight Studio – OpenAPI (OAS) visual editor";
-            homepage = "https://stoplight.io/studio/";
-            license = licenses.unfreeRedistributable;
-            platforms = platforms.linux;
-            maintainers = with maintainers; [];
-        };
-    }
+    extraInstallCommands = ''
+        ls -la ${appimageContents}
+
+        install -m 444 -D ${appimageContents}/${pname}.desktop $out/share/applications/${pname}.desktop
+        install -m 444 -D ${appimageContents}/${pname}.png out/share/icons/hicolor/512x512/apps/${pname}.png
+        substituteInPlace $out/share/applications/${pname}.desktop --replace-fail 'Exec=AppRun' 'Exec=${pname}'
+    '';
+
+    meta = {
+        description = "Stoplight Studio – OpenAPI (OAS) visual editor";
+        homepage = "https://stoplight.io/studio/";
+        license = lib.licenses.unfreeRedistributable;
+        mainProgram = "stoplight-studio";
+        maintainers = with lib.maintainers; [ rhydianjenkins ];
+    };
+}
