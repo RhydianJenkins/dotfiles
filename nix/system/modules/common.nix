93f3726bfcc77650e61ec309dcad3f35914fb72b
Rhydian Jenkins <rhydz@msn.com>
2026-01-12 23:15:35 +0000

    Limit systemd boot configurations to 5

--- nix/system/modules/common.nix
+++ nix/system/modules/common.nix
@@ -4,6 +4,7 @@
     environment.pathsToLink = [ "/libexec" ];
 
     boot.loader.systemd-boot.enable = true;
+    boot.loader.systemd-boot.configurationLimit = 5;
     boot.loader.efi.canTouchEfiVariables = true;
 
     nix = {
