c645707d221040d164b95e9e86227207b3454dca
Rhydian Jenkins <rhydz@msn.com>
2026-01-31 01:49:25 +0000

    Remove cunstom tableplus package for unstable version

--- nix/home/customPkgs/default.nix
+++ nix/home/customPkgs/default.nix
@@ -7,7 +7,6 @@
 final: prev: {
   apix = apix.packages.${system}.default;
   customStoplight = final.callPackage ./stoplight.nix { };
-  customTableplus = final.callPackage ./tableplus.nix { };
   seek = seek.packages.${system}.default;
   unstable = pkgs-unstable;
 }
