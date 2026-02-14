bc10aaa164057dc9bc8d28bd89d3c887a25b2f9f
Rhydian Jenkins <rhydz@mens.com>
2026-02-14 16:53:01 +0000

    Remove esline autocmd

--- nvim/lua/rhydian/autocommands.lua
+++ nvim/lua/rhydian/autocommands.lua
@@ -15,19 +15,3 @@ vim.api.nvim_create_autocmd("BufWritePre", {
         end
     end,
 })
-
-vim.api.nvim_create_autocmd("BufWritePre", {
-    desc = "EsLint fix on save",
-    group = vim.api.nvim_create_augroup("EsLintFormattingGroup", { clear = true }),
-    pattern = {
-        "*.js",
-        "*.jsx",
-        "*.ts",
-        "*.tsx",
-    },
-    callback = function()
-        if vim.fn.executable("EslintFixAll") == 1 then
-            vim.cmd("EslintFixAll")
-        end
-    end,
-})
