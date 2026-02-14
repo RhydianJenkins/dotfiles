9e9bc67ada93ad8e1364a559135ca9183e466f16
Rhydian Jenkins <rhydz@msn.com>
2026-02-14 16:27:27 +0000

    Stylua

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -7,10 +7,30 @@ end
 
 harpoon:setup()
 
-vim.keymap.set("n", "<leader>m", function() harpoon:list():add() end, { desc = "Add mark" })
-vim.keymap.set("n", "<leader>h", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Toggle quick menu" })
-vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end, { desc = "Navigate to mark 1" })
-vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end, { desc = "Navigate to mark 2" })
-vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end, { desc = "Navigate to mark 3" })
-vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end, { desc = "Navigate to mark 4" })
-vim.keymap.set("n", "<leader>5", function() harpoon:list():select(5) end, { desc = "Navigate to mark 5" })
+vim.keymap.set("n", "<leader>m", function()
+    harpoon:list():add()
+end, { desc = "Add mark" })
+
+vim.keymap.set("n", "<leader>h", function()
+    harpoon.ui:toggle_quick_menu(harpoon:list())
+end, { desc = "Toggle quick menu" })
+
+vim.keymap.set("n", "<leader>1", function()
+    harpoon:list():select(1)
+end, { desc = "Navigate to mark 1" })
+
+vim.keymap.set("n", "<leader>2", function()
+    harpoon:list():select(2)
+end, { desc = "Navigate to mark 2" })
+
+vim.keymap.set("n", "<leader>3", function()
+    harpoon:list():select(3)
+end, { desc = "Navigate to mark 3" })
+
+vim.keymap.set("n", "<leader>4", function()
+    harpoon:list():select(4)
+end, { desc = "Navigate to mark 4" })
+
+vim.keymap.set("n", "<leader>5", function()
+    harpoon:list():select(5)
+end, { desc = "Navigate to mark 5" })
