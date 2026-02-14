07a3464e03560029631a1963af4764054293e2c1
Rhydian Jenkins <rhydz@mens.com>
2026-02-14 16:54:01 +0000

    Restore BufReadPost cursor position restoration

--- nvim/lua/rhydian/autocommands.lua
+++ nvim/lua/rhydian/autocommands.lua
@@ -1,3 +1,15 @@
+vim.api.nvim_create_autocmd("BufReadPost", {
+    desc = "Focus cursor on last position",
+    group = vim.api.nvim_create_augroup("FocusCursorGroup", {}),
+    callback = function()
+        local mark = vim.api.nvim_buf_get_mark(0, '"')
+        local lcount = vim.api.nvim_buf_line_count(0)
+        if mark[1] > 0 and mark[1] <= lcount then
+            pcall(vim.api.nvim_win_set_cursor, 0, mark)
+        end
+    end,
+})
+
 vim.api.nvim_create_autocmd("TextYankPost", {
     desc = "Highlight yanked text",
     group = vim.api.nvim_create_augroup("YankHighlight", {}),
