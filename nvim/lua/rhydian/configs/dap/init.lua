468eed5b23a2bac87e836555048d6812be2391ce
Rhydian Jenkins <rhydz@msn.com>
2026-02-15 22:46:11 +0000

    Move dap keybinds to dap config

--- nvim/lua/rhydian/configs/dap/init.lua
+++ nvim/lua/rhydian/configs/dap/init.lua
@@ -1,4 +1,18 @@
+local dap_status, dap = pcall(require, "dap")
 local dapui_status, dapui = pcall(require, "dapui")
+
+if not dap_status then
+    print("dap plugin not found")
+    return
+end
+
+require("rhydian.configs.dap.adapters")
+
+vim.keymap.set("n", "<F5>", dap.toggle_breakpoint, { desc = "Debug toggle breakpoint" })
+vim.keymap.set("n", "<F6>", dap.continue, { desc = "Debug continue" })
+vim.keymap.set("n", "<F9>", dap.step_over, { desc = "Debug step over" })
+vim.keymap.set("n", "<F10>", dap.step_into, { desc = "Debug step into" })
+vim.keymap.set("n", "<F11>", dap.step_out, { desc = "Debug step out" })
 
 if not dapui_status then
     print("dap ui plugin not found")
@@ -7,4 +21,5 @@ end
 
 dapui.setup()
 
-require("rhydian.configs.dap.adapters")
+vim.keymap.set("n", "<F1>", dapui.eval, { desc = "Debug evaluate" })
+vim.keymap.set("n", "<F4>", dapui.toggle, { desc = "Debug toggle" })
