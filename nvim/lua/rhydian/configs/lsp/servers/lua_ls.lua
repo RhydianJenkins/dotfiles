f2a5bb9bfe36cd5cf719a4215cacf341eb15e2c3
Rhydian Jenkins <rhydz@mens.com>
2026-02-15 13:53:45 +0000

    Remove checks for vim.lsp.config

--- nvim/lua/rhydian/configs/lsp/servers/lua_ls.lua
+++ nvim/lua/rhydian/configs/lsp/servers/lua_ls.lua
@@ -1,13 +1,11 @@
-if vim.lsp and vim.lsp.config then
-    vim.lsp.config("lua_ls", {
-        settings = {
-            Lua = {
-                runtime = { version = "LuaJIT" },
-                workspace = {
-                    library = { vim.env.VIMRUNTIME },
-                    checkThirdParty = false,
-                },
+vim.lsp.config("lua_ls", {
+    settings = {
+        Lua = {
+            runtime = { version = "LuaJIT" },
+            workspace = {
+                library = { vim.env.VIMRUNTIME },
+                checkThirdParty = false,
             },
         },
-    })
-end
+    },
+})
