e78a41ce4b2d0935ae72c379644080b4f504825d
Rhydian Jenkins <rhydz@mens.com>
2026-02-15 14:05:20 +0000

    clean diagnostics.lua

--- nvim/lua/rhydian/configs/lsp/diagnostics.lua
+++ nvim/lua/rhydian/configs/lsp/diagnostics.lua
@@ -1,12 +1,14 @@
 local plugins = require("rhydian.configs.lsp.init-plugins")
-local lspconfig = plugins.lspconfig
 local cmp_nvim_lsp = plugins.cmp_nvim_lsp
+local lsp_defaults = plugins.lspconfig.util.default_config
 
 vim.diagnostic.config({
     virtual_text = false,
     float = { border = "rounded" },
 })
 
-local lsp_defaults = lspconfig.util.default_config
-
-lsp_defaults.capabilities = vim.tbl_deep_extend("force", lsp_defaults.capabilities, cmp_nvim_lsp.default_capabilities())
+lsp_defaults.capabilities = vim.tbl_deep_extend(
+    "force",
+    lsp_defaults.capabilities,
+    cmp_nvim_lsp.default_capabilities()
+)
