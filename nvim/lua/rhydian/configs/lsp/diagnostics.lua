f9db8866df0cf8a0a9706b90490bdc6472eff496
Rhydian Jenkins <rhydz@mens.com>
2026-02-16 23:14:51 +0000

    Clean lsp config structure

--- nvim/lua/rhydian/configs/lsp/diagnostics.lua
+++
@@ -1,8 +0,0 @@
-local plugins = require("rhydian.configs.lsp.plugins")
-local lsp_defaults = plugins.lspconfig.util.default_config
-
-vim.diagnostic.config({
-    virtual_text = false,
-})
-
-lsp_defaults.capabilities = require("blink.cmp").get_lsp_capabilities(lsp_defaults.capabilities)
--- nvim/lua/rhydian/configs/lsp/init.lua
+++ nvim/lua/rhydian/configs/lsp/init.lua
@@ -1,3 +1,40 @@
-require("rhydian.configs.lsp.diagnostics")
-require("rhydian.configs.lsp.mason")
+local plugins = require("rhydian.configs.lsp.plugins")
+local blinkcmp = require("blink.cmp")
+local lsp_defaults = plugins.lspconfig.util.default_config
+
+vim.diagnostic.config({ virtual_text = false })
+
+lsp_defaults.capabilities = blinkcmp.get_lsp_capabilities(
+    lsp_defaults.capabilities
+)
+
+plugins.null_ls.setup({
+    sources = {},
+})
+
+plugins.mason.setup()
+
+plugins.mason_null_ls.setup({
+    ensure_installed = {
+        "phpcs",
+    },
+    automatic_setup = true,
+    handlers = {
+        function(source_name, methods)
+            require("mason-null-ls.automatic_setup")(source_name, methods)
+        end,
+        phpcs = function(_, _)
+            local ruleset_exists = vim.fn.filereadable("tests/phpcs-ruleset.xml") == 1
+            local extra_args = ruleset_exists and { "--standard=tests/phpcs-ruleset.xml" } or { "--standard=PSR12" }
+
+            plugins.null_ls.register(plugins.null_ls.builtins.diagnostics.phpcs.with({
+                extra_args = extra_args,
+                method = plugins.null_ls.methods.DIAGNOSTICS_ON_SAVE,
+            }))
+        end,
+    },
+})
+
+plugins.mason_lspconfig.setup()
+
 require("rhydian.configs.lsp.servers")
