4310c6d0f164388f2cbd1571814f5e75a3c7cac7
Rhydian Jenkins <rhydz@msn.com>
2026-05-02 18:31:40 +0100

    Add comment for clarity

--- nix/home/modules/editors.nix
+++ nix/home/modules/editors.nix
@@ -14,10 +14,11 @@
     php84Packages.composer
   ];
 
-  home.file.".vimrc".source = ../../../dotfiles/.vimrc;
+  # make .config/nvim directory writable for vim.pack lockfile
+  home.file.".config/nvim/after".source = ../../../nvim/after;
   home.file.".config/nvim/init.lua".source = ../../../nvim/init.lua;
   home.file.".config/nvim/lua".source = ../../../nvim/lua;
-  home.file.".config/nvim/after".source = ../../../nvim/after;
+  home.file.".vimrc".source = ../../../dotfiles/.vimrc;
 
   home.sessionVariables = {
     EDITOR = "nvim";
