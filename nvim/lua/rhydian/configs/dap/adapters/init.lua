d72c96aa5261a27d366cb1e37c3c2fcf740252df
Rhydian Jenkins <rhydz@msn.com>
2026-06-10 10:06:15 +0100

    Remove php stuff

--- nvim/lua/rhydian/configs/dap/adapters/init.lua
+++ nvim/lua/rhydian/configs/dap/adapters/init.lua
@@ -1,3 +1,2 @@
 require("rhydian.configs.dap.adapters.go")
-require("rhydian.configs.dap.adapters.php")
 require("rhydian.configs.dap.adapters.rust")
--- nvim/lua/rhydian/configs/lsp/init.lua
+++ nvim/lua/rhydian/configs/lsp/init.lua
@@ -13,22 +13,11 @@ plugins.null_ls.setup({
 plugins.mason.setup()
 
 plugins.mason_null_ls.setup({
-    ensure_installed = {
-        "phpcs",
-    },
+    ensure_installed = {},
     automatic_setup = true,
     handlers = {
         function(source_name, methods)
             require("mason-null-ls.automatic_setup")(source_name, methods)
-        end,
-        phpcs = function(_, _)
-            local ruleset_exists = vim.fn.filereadable("tests/phpcs-ruleset.xml") == 1
-            local extra_args = ruleset_exists and { "--standard=tests/phpcs-ruleset.xml" } or { "--standard=PSR12" }
-
-            plugins.null_ls.register(plugins.null_ls.builtins.diagnostics.phpcs.with({
-                extra_args = extra_args,
-                method = plugins.null_ls.methods.DIAGNOSTICS_ON_SAVE,
-            }))
         end,
     },
 })
