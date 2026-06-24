259dd8f09c132eafba3723f1b68ccadd1cc4157f
Rhydian Jenkins <rhydz@msn.com>
2026-06-24 08:41:37 +0100

    remove captive-browser

--- nix/system/modules/common.nix
+++ nix/system/modules/common.nix
@@ -3,12 +3,6 @@
 {
   environment.pathsToLink = [ "/libexec" ];
 
-  programs.captive-browser = {
-      enable = true;
-      interface = "$(ip route show default | awk '{print $5}' | head -n1)";
-      dhcp-dns = "${pkgs.networkmanager}/bin/nmcli dev show $(ip route show default | awk '{print $5}' | head -n1) | grep 'IP4.DNS' | awk '{print $2}' | head -n1";
-  };
-
   boot.loader.systemd-boot.enable = true;
   boot.loader.systemd-boot.configurationLimit = 5;
   boot.loader.efi.canTouchEfiVariables = true;
